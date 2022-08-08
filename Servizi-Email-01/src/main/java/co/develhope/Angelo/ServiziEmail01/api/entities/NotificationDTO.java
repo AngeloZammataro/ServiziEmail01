205586fc8740466da83ee4932d526c37001801da
Angelo Zammataro <juba.kem@gmail.com>
2022-08-08 10:24:00 +0200

    First Commit: Servizi - Email 01 ver.0.1

--- Servizi-Email-01/src/main/java/co/develhope/Angelo/ServiziEmail01/api/entities/NotificationDTO.java
+++ Servizi-Email-01/src/main/java/co/develhope/Angelo/ServiziEmail01/api/entities/NotificationDTO.java
@@ -1,17 +1,42 @@
 package co.develhope.Angelo.ServiziEmail01.api.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 //defines a NotificationDTO
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class NotificationDTO {
 
     private String contactId;
     private String title;
     private String text;
+
+    public NotificationDTO() {
+    }
+
+    public NotificationDTO(String contactId, String title, String text) {
+        this.contactId = contactId;
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
